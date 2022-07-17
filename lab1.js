605c3f7acf782843a6604b5c331d1097ce8cc7ff
Renzo Silva <renzoasilva@gmail.com>
2022-07-17 16:07:40 -0400

    Greater number js updated

--- lab1.js
+++ lab1.js
@@ -2,3 +2,20 @@
 // const toText = String(num)
 // console.log(toText)
 // console.log('js')
+
+// LAB 1 - What number's bigger?
+// Write a function named greaterNum that:
+// - takes 2 arguments, both numbers.
+// - returns whichever number is the greater (higher) number.
+// Call that function 2 times with different number pairs, and log the output
+// to make sure it works (e.g. "The greater number of 5 and 10 is 10.").
+
+function greaterNum (numA, numB) {
+    if (numA > numB) {
+    console.log("The greater number of ", + numA + " and ", + numB + " is ", + numA)
+    } else {
+    console.log("The greater number of ", + numA + " and ", + numB + " is ", + numB)
+}
+}
+greaterNum(3, 5)
+greaterNum(8, 5)
