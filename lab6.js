3caadb43faee04cf9b195f08928b3f184dc670d9
Renzo Silva <renzoasilva@gmail.com>
2022-07-17 18:34:27 -0400

    Largest number

---
+++ lab6.js
@@ -0,0 +1,17 @@
+//-- LAB 6 - Find the largest of two number
+  /* Function `findLargest()` finds the largest between two number by using ">"
+     and "=" operator in JavaScript.
+     Print num1 is the largest if num1>num2.
+     Print num2 is the largest if num1<num2.
+     Else print num1 and num2 are equal when num1==num2.*/
+
+     function findLargest(num1, num2) {
+        if (num1 > num2) {
+            return `${num1} is the largest`
+        } else if (num1 < num2) {
+            return `${num2} is the largest`
+        } else (num1 === num2) 
+            return `${num1} and ${num2} are equal`
+     }
+     
+     console.log(findLargest(56, 56))
