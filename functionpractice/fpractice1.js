624aa215ceb14d9620150bb024f25f4a14f20416
Renzo Silva <renzoasilva@gmail.com>
2022-07-17 13:12:34 -0400

    Functions and loops

---
+++ functionpractice/fpractice1.js
@@ -0,0 +1,15 @@
+// //first practice
+
+// function firstp (name, lastn) {
+//     console.log("Hello", " " + name + " " + lastn)
+
+
+//}
+//firstp("Renzo", "Silva")
+
+function adder (mango) {
+    for (let i = 0; i < mango.length; i++)
+    console.log(mango[i]);
+
+}
+adder(["car", "dog", "city"])
