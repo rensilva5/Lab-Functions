dbfe2a97c50753ebf07a895eb1eebb8c3e20bc1b
Renzo Silva <renzoasilva@gmail.com>
2022-07-17 18:17:39 -0400

    Pluralizer has been updated

--- lab4.js
+++ lab4.js
@@ -1,8 +1,15 @@
+// LAB 4 -  The Pluralizer
+// Write a function named pluralize that:
+// - takes 2 arguments, a noun and a number.
+// - returns the number and pluralized form, like "5 cats" or "1 dog".
+// Call that function for a few different scores and log the result to make sure it works.
+// Bonus: Make it handle a few collective nouns like "sheep" and "geese".
 
-let function pluralize(animal, num){
-    if (num = 1){
-    return `${animal},"s" ${num}`
+function pluralizer(number, noun) {
+    if (number === 1){
+    console.log(`${number} ${noun}`)
 } else {
-    return `${animal} ${num}`
+    console.log(`${number} ${noun}s`)
 }
-pluralize(cat, 1);
+}
+pluralizer(4, "cat")
