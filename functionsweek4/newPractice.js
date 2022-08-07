0e9bab44d0ed49ebf3ccfb9aa22b49236fee138c
rensilva5 <renzoasilva@gmail.com>
2022-08-06 22:20:37 -0400

    Some practice and fixes

---
+++ functionsweek4/newPractice.js
@@ -0,0 +1,17 @@
+function color (){
+    return 'red';
+}
+//console.log(color())
+
+// --
+function repeatStr (str){
+    return str + str;
+  }
+  //console.log(repeatStr('Wave'))
+
+  function repeat (text){
+    let result = text + text
+    //console.log(result)
+    return result
+    }
+console.log(repeat('Universe'))
